f238fdf04419f72c8e7fbcfd52620d5cc5d1f852
naganw <116052739+naganw@users.noreply.github.com>
2025-01-16 13:45:15 +0600

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+# # print('2 x 2 =', 2*2)
+# # print('2 x 3 =', 2*3)
+# # print('2 x 4 =', 2*4)
+# # print('2 x 5 =', 2*5)
+# #
+# # name = input ("your name : ")
+# # age = input('your age : ')
+# #
+# # print(f'your name: {name} and your age:{age}' )
+#
+# name = input("Tom")
+#
+# print(name)
+#
+
+age = int(input('enter your age:'))
+print(age)
+print(type(age))
+age=34
+myAge=str(age)
+age=34.0
