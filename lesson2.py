123525e7fed5b3401bf234936ec4165754690935
naganw <116052739+naganw@users.noreply.github.com>
2025-01-16 13:46:35 +0600

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,43 @@
+import random
+
+'''
+ Name = input('youre name: ')
+ Name = input('youre name: ')
+
+ print(Name)
+'''
+'''Name = input('name:')
+surName = input('surName:')
+fullName =input('fullName:')
+
+print(fullName)
+'''
+'''r = 15
+a = 3.14 * r * r
+print(a)'''
+
+'''num1 = 10
+num2 = 8
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)'''
+
+'''c= float(input('C='))
+f= 9/5 * c + 32
+print(f)'''
+
+'''a= 10.6
+print(a)
+b= int(a)
+print(b)'''
+
+# num1 = float(input("Enter a number: "))
+# num2 = float(input("Enter another number: "))
+#
+# result_float = float(num1 + num2)
+# result_int = int(num1 * num2)
+#
+# print(f"original result: {result_float},integer number: {result_int}")
